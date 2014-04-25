33c1aa6ebd9c96c9470000878191f2af44409901
Justin Thibeault <justin.thibeault@gmail.com>
2014-04-25 06:37:16 -0700

    add planet functionality

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -2,4 +2,15 @@ class PlanetsController < ApplicationController
   def index
     @planets = Planet.all
   end
+
+  def new 
+    
+  end
+
+  def create
+    @planet = Planet.new(params[:planet])
+
+    @planet.save
+    redirect_to :action => "index"
+  end
 end
