14aac47d38c151fd2c57f44c7a629ae1199bb447
Justin Thibeault <justin.thibeault@gmail.com>
2014-04-26 14:26:11 -0700

    switched views and model over to classification

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,5 +1,5 @@
 class Ship < ActiveRecord::Base
-  attr_accessible :name, :type, :weight, :num_engines, :captains_id, :engine_id, :planet_id
+  attr_accessible :name, :classification, :weight, :num_engines, :captains_id, :engine_id, :planet_id
 
   has_one :planet
   has_one :captain
